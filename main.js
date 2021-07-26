05b6209d80acf22c19738094bb4e2e86c2e9bfee
Victor Tran <vtranwd@gmail.com>
2021-07-26 15:35:03 -0700

    added show hide function

--- main.js
+++ main.js
@@ -4,18 +4,42 @@
 // TODO 2: REVERSE THE DISPLAY ORDER OF THE FRIENDS
 // TODO 3: GO TO https://avataaars.io TO GET A NEW AVATAR
 // AND ADD ANOTHER FRIEND TO THIS ARRAY
+// TODO 4: CLICK ON THE "FRIEND-CARD" AND:
+//   1 - DISPLAY THAT FRIEND'S INFORMATION ON THE DIV WITH ID="js-friend-info"
+//   2 - HIDES THE FRIEND-LIST
+
 const friends = [
       {
         avatar:
           "https://avataaars.io/?avatarStyle=Circle&topType=LongHairBob&accessoriesType=Blank&hairColor=Platinum&facialHairType=Blank&facialHairColor=Red&clotheType=ShirtVNeck&clotheColor=Gray01&eyeType=Happy&eyebrowType=RaisedExcited&mouthType=Twinkle&skinColor=Light",
         name: "Alexa",
-        mood: "Always happy"
+        mood: "Always happy",
+        age: 55,
+        hideShowButton: "hide/show"
       },
       {
         avatar:
           "https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&accessoriesType=Prescription01&hairColor=PastelPink&facialHairType=BeardMedium&facialHairColor=BlondeGolden&clotheType=GraphicShirt&clotheColor=Blue02&graphicType=Skull&eyeType=Surprised&eyebrowType=RaisedExcitedNatural&mouthType=Grimace&skinColor=Black",
         name: "Jimmy",
-        mood: "Always worried"
+        mood: "Always worried",
+        age: 9001,
+        hideShowButton: "hide/show"
+      },
+      {
+        avatar:
+          "https://avataaars.io/?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Gray01&clotheType=ShirtCrewNeck&eyeType=WinkWacky&eyebrowType=RaisedExcited&facialHairColor=BlondeGolden&facialHairType=Blank&hairColor=Platinum&hatColor=PastelBlue&mouthType=Grimace&skinColor=Yellow&topType=WinterHat1",
+        name: "Brad",
+        mood: "Always annoyed",
+        age: 34,
+        hideShowButton: "hide/show"
+      },
+      {
+        avatar:
+          "https://avataaars.io/?accessoriesType=Kurt&avatarStyle=Circle&clotheType=ShirtScoopNeck&eyeType=Hearts&eyebrowType=RaisedExcited&facialHairType=MoustacheFancy&hairColor=Red&mouthType=Sad&skinColor=Tanned&topType=Hijab",
+        name: "Cass",
+        mood: "Always amused",
+        age: 899,
+        hideShowButton: "hide/show"
       },
     ];
 
@@ -26,7 +50,7 @@ const friends = [
       .map(
         (friend) =>
           `
-    <div class="friend">
+    <div class="friend-card">
         <div class="avatar">
           <img
             src="${friend.avatar}"
@@ -34,7 +58,20 @@ const friends = [
         </div>
         <div class="name">${friend.name}</div>
         <div class="mood">${friend.mood}</div>
+        <div class="age">${friend.age}</div>
+        <div class="hideShowButton">${friend.hideShowButton}</div>
       </div>
     `
       )
+      .reverse()
       .join("");
+
+      function hideFriends() {
+        var x = document.getElementById("ishabutton");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+      
